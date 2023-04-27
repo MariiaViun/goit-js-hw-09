0532300b8230fc98f4ab0c4095505e182fc3abb7
Mariia <lukrainka03@gmail.com>
2023-04-27 19:15:11 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector("body");
+let timer = null;
+
+// buttonStop.setAttribute("disabled", "disabled");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener("click", () => {
+    buttonStart.disabled = true;
+    timer = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    buttonStop.disabled = false;
+});
+
+
+
+buttonStop.addEventListener("click", () => {
+    clearInterval(timer);
+    buttonStart.disabled = false;
+})
 
